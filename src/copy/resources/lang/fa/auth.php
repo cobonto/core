3d3a5a70d1a6524b8ee00e009eb106710dfe13a7
msharif ahrari <msharif.ahrari@gmail.com>
2017-01-05 21:14:45 +0330

    fix bug in english auth

--- src/copy/resources/lang/fa/auth.php
+++ src/copy/resources/lang/fa/auth.php
@@ -1,13 +1,14 @@
-<?php 
- return array (
-  'failed' => 'These credentials do not match our records.',
-  'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-  'welcome_title' => 'Welcome to admin panel',
-  'sign_in_title' => 'Sign in to start your session',
-  'email' => 'ایمیل',
-  'password' => 'کلمه عبور',
-  'remember' => 'من را به خاطر داشته باش',
-  'sign_in_btn' => 'ورود',
-  'forget_passwd' => 'کلمه عبورم را فراموش کردم',
-  'copy_right' => '2016 شرکت DMT',
+<?php
+return array(
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'welcome_title'=>'Welcome to admin panel',
+    'sign_in_title'=>'Sign in to start your session',
+    'email'=>'Email',
+    'password'=>'Password',
+    'remember'=>'Remember Me',
+    'sign_in_btn'=>'Sign In',
+    'forget_passwd'=>'I forget my password',
+    'copy_right'=>'2017 DMT company LTD'
 );
